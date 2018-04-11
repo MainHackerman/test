caeac37db508ac618debad09eb2e63b5625c007c
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-04-11 14:05:07 +0200

    Test working - portal integration not included

--- test.py
+++ test.py
@@ -1,11 +1,40 @@
-string = "Pepa  zaplati za  10.0  piv  1000  korun."
+string = "zaplati  Pepa  za 1000.0 piv 10 korun."
 parts = string.split()
 isnum = list()
+nums = list()
+
+name = "Pepa"
+solution = [1000.0, 10.0]
+correct = bool()
+correct_name = False
 
 for item in parts:
+    if item == name:
+        correct_name = True
     try:
         float(item)
         isnum.append(True)
     except:
         isnum.append(False)
 
+for it in parts:
+    try:
+        float(it)
+        nums.append(float(it))
+    except:
+        pass
+
+# JUDJING
+if len(nums) != 2:
+    print("FAIL - Wrong output string -not enaught numbers")
+    quit()
+
+if nums[0] == solution[0] and nums[1] == solution[1]:
+    correct = True
+elif nums[0] == solution[1] and nums[1] == solution[0]:
+    correct = True
+else:
+    correct = False
+
+print(correct)
+print(correct_name)
