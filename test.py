d7dc6b9d55fb6b1a1957727b51265dcbe006e1ba
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-04-11 13:40:32 +0200

    Testing number using try/except

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
 string = "Pepa  zaplati za  10.0  piv  1000  korun."
 parts = string.split()
-
-
-def isnumber(teststr):
-    return teststr.isdigit() or teststr.isnumeric() or teststr.isdecimal()
-
-
 isnum = list()
-for item in parts:
-    isnum.append(isnumber(item))
-    print(item, item.isdigit())
 
-print(isnum)
-decim = "10.02"
-print(decim.isdecimal())
+for item in parts:
+    try:
+        float(item)
+        isnum.append(True)
+    except:
+        isnum.append(False)
+
